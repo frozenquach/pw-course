dcc2598312ac79f91272bc68f5c10f3ae20d04a0
ducquach <frozenquach@gmail.com>
2026-03-04 21:57:14 +0700

    bai tap 2

---
+++ tests/lesson-02/ex2.js
@@ -0,0 +1,4 @@
+//const myName = "Alex"; //lỗi
+let myName = "Alex"; //sửa
+myName = "Nagi";
+console.log(myName);
---
+++ tests/lesson-02/ex3.js
@@ -0,0 +1,5 @@
+const a=true
+const b=false
+const c=true
+console.log(a && b && c)  //false
+console.log(a && b || c) //true
